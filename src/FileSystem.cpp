f3744b1106df4fd9577b16c00adee805f9f0175b
Edward Rudd <urkle@outoforder.cc>
2015-02-26 16:19:15 -0500

    implement Win32 Filesystem methods

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -8,6 +8,9 @@
 
 #include "FileSystem.h"
 
+#ifndef WIN32
+#define PATH_SEPARATOR "/"
+
 #include <unistd.h>
 
 FILE* FileSystem::open(const std::string &path, const std::string& mode)
@@ -20,14 +23,34 @@ bool FileSystem::exists(const std::string &path)
 {
     return access(path.c_str(), F_OK) == 0;
 }
+#else // the windows code-path
+#define PATH_SEPARATOR "\\"
+
+#include <io.h>
+
+bool FileSystem::exists(const std::string &path)
+{
+	return _access(path.c_str(), 0) == 0;
+}
+
+
+FILE* FileSystem::open(const std::string &path, const std::string& mode)
+{
+    FILE* fp = 0;
+	fopen_s(&fp, path.c_str(), mode.c_str());
+    return fp;
+}
+#endif
+
 
 std::string FileSystem::join_path(const std::string &dir, const std::string &file)
 {
     std::string ret = dir;
-    if (ret[ret.length() - 1] != '/') {
-        ret += "/";
+	char last_c = ret[ret.length() - 1];
+    if (last_c != '/' && last_c != '\\') {
+        ret += PATH_SEPARATOR;
     }
-    if (file[0] == '/') {
+    if (file[0] == '/' || file[0] == '\\') {
         ret += file.substr(1);
     } else {
         ret += file;
