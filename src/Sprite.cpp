84062553480e7bfba07e5476cb49c4f5e62f4884
Edward Rudd <urkle@outoforder.cc>
2015-03-03 21:44:45 -0500

    add angle rotation support to sprite

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -11,13 +11,14 @@
 
 Sprite::Sprite(TextureRef texture)
     : m_texture(texture),
-    m_size(m_texture->size().as<float>())
+    m_size(m_texture->size().as<float>()),
+    m_angle(0.0f)
 {
 }
 
 void Sprite::render(Renderer& rend)
 {
-    rend.draw_texture(m_texture, (m_position - m_center).as<int>());
+    rend.draw_texture(m_texture, (m_position - m_center).as<int>(), m_angle);
 }
 
 void Sprite::move_position_by(const Vector2f &adjust_position)
--- src/Sprite.h
+++ src/Sprite.h
@@ -18,14 +18,18 @@ class Sprite : public Renderable {
     Vector2f m_position;
     Vector2f m_size;
     Vector2f m_center;
+    float m_angle;
 public:
     Sprite(TextureRef m_texture);
     virtual void render(Renderer& rend);
 
     void set_position(const Vector2f& position) { m_position = position; }
     const Vector2f& position() const { return m_position; }
-
     void move_position_by(const Vector2f& adjust_position);
+
+    void set_angle(float angle) { m_angle = angle; }
+    float angle() { return m_angle; }
+    void rotate_by(float angle) { m_angle += angle; }
 
     void set_center(const Vector2f& center) { m_center = center; }
     const Vector2f& center() const { return m_center; }
