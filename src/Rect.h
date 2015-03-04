2b6705a1901a49ad98529fd629f5ea0441ff82f3
Edward Rudd <urkle@outoforder.cc>
2015-03-04 13:45:32 -0500

    add intersection check to Rect

--- src/Rect.h
+++ src/Rect.h
@@ -19,15 +19,28 @@ struct Rect {
     Rect(const Vector2i& size) : x(0), y(0), w(size.x), h(size.y) {}
     Rect(const Vector2i& position, const Vector2i& size) : x(position.x), y(position.y), w(size.x), h(size.y) {}
 
-    Vector2i bottom_right() { return Vector2i(x + w, y + h); }
+    Vector2i bottom_right() const { return Vector2i(x + w, y + h); }
 
-    bool contains(const Vector2i& point) {
+    bool empty() const {
+        return w == 0 && h == 0;
+    }
+
+    bool contains(const Vector2i& point) const {
         Vector2i br = bottom_right();
 
         if (point.x >= x && point.y >= y
             && point.x < br.x && point.y < br.y) {
             return true;
         }
+        return false;
+    }
+
+    bool intersects(const Rect& r) {
+        if (empty() || r.empty()) return false;
+
+        if (r.x < x + w && x < r.x + r.w && r.y < y + h)
+            return y < r.y + r.h;
+
         return false;
     }
 };
