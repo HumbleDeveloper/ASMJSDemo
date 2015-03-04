fd815df8c2a1092bbafbbaf77f48865ccb85de24
Edward Rudd <urkle@outoforder.cc>
2015-03-03 21:45:00 -0500

    add some more helpers to rect

--- src/Rect.h
+++ src/Rect.h
@@ -16,5 +16,18 @@ struct Rect {
 
     Rect() : x(0), y(0), w(0), h(0) {}
     Rect(int _x, int _y, int _w, int _h) : x(_x), y(_y), w(_w), h(_h) {}
+    Rect(const Vector2i& size) : x(0), y(0), w(size.x), h(size.y) {}
     Rect(const Vector2i& position, const Vector2i& size) : x(position.x), y(position.y), w(size.x), h(size.y) {}
+
+    Vector2i bottom_right() { return Vector2i(x + w, y + h); }
+
+    bool contains(const Vector2i& point) {
+        Vector2i br = bottom_right();
+
+        if (point.x >= x && point.y >= y
+            && point.x < br.x && point.y < br.y) {
+            return true;
+        }
+        return false;
+    }
 };
