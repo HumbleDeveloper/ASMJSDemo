3cf128bf383667bf73b53da17469e47b681b4fe7
Edward Rudd <urkle@outoforder.cc>
2015-04-02 23:12:22 -0400

    fix type of color var.

--- src/Color.h
+++ src/Color.h
@@ -9,7 +9,7 @@
 #pragma once
 
 struct Color {
-    char r,g,b,a;
+    unsigned char r,g,b,a;
 
     Color() : r(0), g(0), b(0), a(255) {}
     Color(char _r, char _g, char _b, char _a = 0) : r(_r), g(_g), b(_b), a(_a) {}
